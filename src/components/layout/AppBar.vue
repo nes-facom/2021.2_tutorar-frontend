8e7d8f0367ff2b32c69499dc6abf96c8a6b79277
Vinicius Valente <viniciusorejana@yahoo.com.br>
2020-12-02 19:37:10 -0400

    Todo NavBar [update]

--- src/components/layout/AppBar.vue
+++ src/components/layout/AppBar.vue
@@ -98,6 +98,7 @@ export default class AppBar extends Vue {
           Caso o tutor não esteja na home (perfil), disponiblizar um botão indicando o "Painel de Controle".
           Caso o professor não esteja na home (busca de tutoria), disponibilizar um botão indicando "Buscar Tutoria".
           Caso o coordenador não esteja na home (listagem de usuários), disponibilizar um botão indicando "Lista de Usuários".
+          Caso o coordenador esteja na home (listagem de usuários), disponiblizar um botão indicando "Relatórios".
 -->
     <v-breadcrumbs :items="routeSections" divider="/" class="hidden-sm-and-down" large />
 
