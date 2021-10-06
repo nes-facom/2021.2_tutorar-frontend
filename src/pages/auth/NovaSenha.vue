079cc865f2bc9f158838246674a75fef01a990e2
Larissa <larissa.fraga@ufms.br>
2021-10-05 22:54:03 -0400

    implementação da parte de criar uma nova senha, parte está funcional

--- src/pages/auth/NovaSenha.vue
+++ src/pages/auth/NovaSenha.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import AppBarCadastro from "@/components/auth/AppBarCadastro.vue"
 import { Component, Vue } from "vue-property-decorator"
+import { resetPasswordService } from "@/api/auth/recover-password"
 
 @Component({
   name: "NovaSenha",
@@ -10,6 +11,23 @@ export default class NovaSenha extends Vue {
   recuperando = true
 
   canSubmit = false
+
+  updatePassword() {
+    const password = document.getElementById("nova-senha")?.innerHTML
+    const newPassword = document.getElementById("confirma-senha")?.innerHTML
+  
+    const token = this.$route.query.token
+    console.log(token)
+    console.log(password)
+    console.log(newPassword)
+
+    if(password && newPassword) {
+      if(!resetPasswordService(token.toString(), password, newPassword)) {
+        console.log("Não foi possível fazer a requisição")
+      }
+    }
+
+  }
 
 }
 </script>
@@ -23,9 +41,9 @@ export default class NovaSenha extends Vue {
           <v-card-subtitle class="grey--text text--darken-2 px-0"> informe sua nova senha </v-card-subtitle>
 
           <v-form v-model="canSubmit">
-            <v-text-field outlined placeholder="Nova senha" type="password" />
+            <v-text-field id="nova-senha" placeholder="Nova senha" type="password" />
 
-            <v-text-field outlined placeholder="Confirmação" type="password" />
+            <v-text-field id="confirma-senha" outlined placeholder="Confirmação" type="password" ref="conf-password"/>
           </v-form>
 
           <v-card-actions class="pa-0 mx-0 mt-0 justify-center">
@@ -33,6 +51,7 @@ export default class NovaSenha extends Vue {
               :disabled="!canSubmit"
               color="blue lighten-1"
               class="white--text px-4 elevation-2"
+              @click="updatePassword()"
             >
               <span>Alterar Senha</span>
             </v-btn>
