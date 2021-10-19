baa0167411d79e419040e67c2c9a15318438f4e6
Vinicius Marchi <viniciuspmarchi@gmail.com>
2021-10-19 19:42:51 -0400

    Corrigindo deploy no Heroku

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express')
 var path = require('path')
 var serveStatic = require('serve-static')
-
+var history = require('connect-history-api-fallback')
 var app = express()
+
+app.use(history({
+    // verbose: true
+}));
+
 app.use(serveStatic(path.join(__dirname, 'dist')))
 
-var port = process.env.PORT || 3000
-app.listen(port)
-console.log('Servidor express web iniciado na porta ' + port)
+// var port = process.env.PORT || 3000
+app.set('port',  (process.env.PORT || 3000))
+
+app.listen(app.get('port'), () => {
+    console.log('Servidor express web iniciado na porta ' + port)
+})
