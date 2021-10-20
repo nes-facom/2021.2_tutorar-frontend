112da145907454cc30273142ec3a8c9fbd2b2fd3
Vinicius Marchi <viniciuspmarchi@gmail.com>
2021-10-19 20:18:36 -0400

    Corrigindo deploy no Heroku

--- server.js
+++ server.js
@@ -1,21 +1,6 @@
-
-const express = require('express')
-const path = require('path')
-const history = require('connect-history-api-fallback')
-// ^ middleware to redirect all URLs to index.html
-
-const app = express()
-const staticFileMiddleware = express.static(path.join(__dirname, '/dist'))
-
-app.use(staticFileMiddleware)
-app.use(history())
-app.use(staticFileMiddleware)
-// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
-
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'))
-})
-
-app.listen(8080, function () {
-  console.log( 'Express serving on 8080!' )
-})
+const express = require('express');
+const history = require('connect-history-api-fallback');const app = express();
+app.use(history());
+app.use(express.static('src'));app.get('/', (req, res) => {
+  res.sendFile('src/index.html');
+});app.listen(3000, () => console.log('server started'));
