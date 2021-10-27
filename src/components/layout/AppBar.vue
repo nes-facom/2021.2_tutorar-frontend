d0edc57e81d9bca61e9011ac7109cd865cc9d074
Luis Gustavo <lgrotrab@gmail.com>
2021-10-26 23:32:55 -0300

    Apagando dados não usados e breadcrumb

--- src/components/layout/AppBar.vue
+++ src/components/layout/AppBar.vue
@@ -6,11 +6,7 @@ import AppBarUserMenu from "@/components/layout/AppBarUserMenu.vue"
 
 import Theme from "@/store/modules/theme"
 
-interface Clock {
-  data: string
-  hora: string
-  timerId: number | null
-}
+
 
 @Component({
   name: "AppBar",
@@ -21,7 +17,6 @@ interface Clock {
 export default class AppBar extends Vue {
   private themeModule = getModule(Theme, this.$store)
 
-  clock: Clock = { data: "", hora: "", timerId: null }
 
   get appBar() {
     return this.themeModule.isAppBarVisible
@@ -29,39 +24,6 @@ export default class AppBar extends Vue {
 
   set appBar(value: boolean) {
     this.themeModule.SET_APP_BAR(value)
-  }
-
-  // Se a rota tem um nome que não é o '*' de fallback 404
-  get routeSections() {
-    return this.$route?.name && this.$route?.name !== "*"
-      ? this.$route.name
-          .replace(/-/g, " ")
-          .split("_")
-          // Se começa com ":" é um parametro de rota e deve ser ignorado
-          .filter(x => !x.startsWith(":"))
-          .map(v => ({ link: false, text: v }))
-      : []
-  }
-
-  updateClock() {
-    const date = new Date()
-    const ano = date.getFullYear()
-    const mes = this.padClock(date.getMonth() + 1)
-    const dia = this.padClock(date.getDate())
-    const hora = this.padClock(date.getHours())
-    const minuto = this.padClock(date.getMinutes())
-
-    this.clock.data = `${dia}/${mes}/${ano}`
-    this.clock.hora = `${hora}:${minuto}`
-  }
-
-  padClock(v: number | string) {
-    return String(v).padStart(2, "0")
-  }
-
-  mounted() {
-    this.updateClock()
-    this.clock.timerId = setInterval(this.updateClock, 5000)
   }
 }
 </script>
@@ -83,7 +45,6 @@ export default class AppBar extends Vue {
       contain
       @click="$router.push('/home')"
     />
-    <v-breadcrumbs :items="routeSections" divider="/" class="hidden-sm-and-down" large />
 
     <v-spacer />
 
